4278db3d6cd62d948cc73584026b554a8904b758
LiisaKotilainen <liisa.kotilainen@edu.turkuamk.fi>
2024-12-26 16:16:51 +0200

    4.15 blogilistan laajennus, step3

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const config = require("./utils/config"); 
-const express = require("express"); 
-const app = express(); 
-const cors = require("cors"); // Lataa CORS
-const blogsRouter = require("./controllers/blogs"); 
-const middleware = require("./utils/middleware"); 
-const logger = require("./utils/logger"); 
-const mongoose = require("mongoose"); 
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+const usersRouter = require("./controllers/users");
 
-mongoose.set("strictQuery", false); // Aseta Mongoose-virheenkäsittely
+mongoose.set("strictQuery", false); // Mongoose-virheenkäsittely
 
-// MongoDB-yhteyden muodostaminen
+// MongoDB-yhteys
 logger.info("connecting to", config.MONGODB_URI);
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
-    logger.info("connected to MongoDB"); 
+    logger.info("connected to MongoDB");
   })
   .catch((error) => {
     logger.error("error connection to MongoDB:", error.message);
@@ -22,14 +23,15 @@ mongoose
 
 // Middlewaret
 app.use(cors());
-app.use(express.json()); 
-app.use(middleware.requestLogger); 
+app.use(express.json());
+app.use(middleware.requestLogger);
 
 // Reitit
-app.use("/api/blogs", blogsRouter); 
+app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 // Virheenkäsittely
-app.use(middleware.unknownEndpoint); 
-app.use(middleware.errorHandler); 
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
